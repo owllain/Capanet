b000b311d637a9888efc16c9839b8541f5c64226
Enrique Cascante <alvaro.cascante@uned.cr>
2024-01-25 04:12:56 -0600

    Proyecto prototipo capa

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'class',
+
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+				pulse: {
+					'0%, 100%': { opacity: 1 },
+					'80%': { opacity: 0.5 },
+				},
+			},
+			animation: {
+				pulse: 'pulse 2s infinite',
+			},
+		},
 	},
 	plugins: [],
-}
+};
+
